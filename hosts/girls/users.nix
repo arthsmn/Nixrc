2ec84a01d1cec0b8e2f661900d3516333213cc91
arthsmn <arthsmn@proton.me>
2024-07-15 17:59:01 -0300

    python lsp

--- hosts/girls/users.nix
+++ hosts/girls/users.nix
@@ -34,7 +34,7 @@
       nix-init
       nix-your-shell
       ocrmypdf
-      python-lsp-server
+      python312Packages.python-lsp-server
       # TODO: usar python padrão quando a versão for >= 3.13
       python313
       ripgrep
