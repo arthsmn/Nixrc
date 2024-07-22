49f67e0fd6df14fa2537b869d60d3159594d72e5
arthsmn <arthsmn@proton.me>
2024-07-22 19:23:49 -0300

    até mais, segredos no home-manager

--- homes/arthur/default.nix
+++ homes/arthur/default.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   outputs,
   ...
 }: {
@@ -7,9 +6,7 @@
     [
       ./dconf.nix
       ./misc.nix
-      ./secrets.nix
       ./shell.nix
     ]
-    ++ (builtins.attrValues outputs.homeManagerModules)
-    ++ [inputs.sops-nix.homeManagerModules.sops];
+    ++ (builtins.attrValues outputs.homeManagerModules);
 }
